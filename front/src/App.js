45b05fe4da1ea4fb2c8f113c8696ce314055b2c0
aSempruch <32146438+aSempruch@users.noreply.github.com>
2018-06-22 19:42:40 -0400

    Working without dynamic word update in typefield

--- front/src/App.js
+++ front/src/App.js
@@ -1,20 +1,117 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { TextField, AppBar, Toolbar, Typography, Paper } from '@material-ui/core'
 
 class App extends Component {
+
+  state = {
+    currentIndex: 0,
+    currentWordProg: '',
+    wordCount: 15,
+    text: 'How much wood would a wood chuck chuck if a wood chuck could chuck wood',
+  }
+
+  action = {
+    updateWordProg: (val) => { this.setState({currentWordProg: val}) },
+    updateIndex: (val) => { this.setState({currentIndex: val}) },
+    getWordProg: _ => ( this.state.currentWordProg ),
+    getIndex: _ => ( this.state.currentIndex ),
+    getText: _ => ( this.state.text ),
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <MyAppBar/>
+        <Paper id='GameContainer'>
+          <GameTextBox action={ this.action }/>
+          <TypeBox action={ this.action }/>
+        </Paper>
       </div>
     );
+  }
+}
+
+class MyAppBar extends Component {
+  render(){
+    return(
+      <AppBar>
+        <Toolbar>
+          <Typography variant='title' color='inherit'>
+            Typing Game
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    )
+  }
+}
+
+class GameTextBox extends Component {
+
+  renderText = _ => {
+    const { action } = this.props;
+    const currentIndex = action.getIndex(), currentWord = action.getWordProg(), text = action.getText();
+    var result = [];
+    result.push(<span id='Typed' key='typed'><u>{text.substring(0, currentIndex)}</u></span>);
+    if(currentIndex < currentWord.length){
+      console.log('render => incorrect')
+      result.push(<span id='Incorrect' key='incorrect'><u>{text.substring(currentIndex, currentWord.length)}</u></span>);
+    }
+    result.push(<span id='Untyped' key='untyped'>{text.substring(currentWord.length, text.length)}</span>);
+    return result;
+  }
+
+  render(){
+    const { renderText } = this;
+    return(
+      <div id='GameTextBox'>
+        {renderText()}
+      </div>
+    )
+  }
+}
+
+class TypeBox extends Component {
+
+  state = {
+    value: '',
+  }
+
+  handleChange = (e) => {
+    const { action } = this.props;
+    this.setState({value: e.target.value});
+    const currentIndex = action.getIndex(), currentWord = e.target.value, text = action.getText();
+
+    /* Backspace */
+    if(currentWord.length < action.getWordProg().length){
+      console.log("Backspace");
+      action.updateIndex(currentWord.length);
+      action.updateWordProg(currentWord);
+    }
+    /* New Text Matches Target Text */
+    else if(currentWord.substring(currentIndex) === text.substring(currentIndex, currentWord.length)){
+      console.log("Match");
+      action.updateIndex(currentWord.length)
+      action.updateWordProg(currentWord)
+    }
+    /* New Text is Wrong */
+    else{
+      console.log("Incorrect");
+      action.updateWordProg(currentWord);
+    }
+  }
+
+  render(){
+    const { handleChange } = this;
+    const { value } = this.state;
+    return(
+      <TextField
+        id='TypeBox'
+        label='Type!'
+        value={value}
+        onChange={handleChange}
+      />
+    )
   }
 }
 
